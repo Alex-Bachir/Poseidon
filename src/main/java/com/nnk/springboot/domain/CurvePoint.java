7a4ab03904f1a5b1f5522decabfa1228d82a9d7b
Alex.Bachir <alex.bachir@laplateforme.io>
2025-12-03 14:42:50 +0100

    first commit

---
+++ src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -0,0 +1,25 @@
+package com.nnk.springboot.domain;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@Table(name = "curvepoint")
+public class CurvePoint {
+    // TODO: Map columns in data table CURVEPOINT with corresponding java fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer curveId;
+    private Timestamp asOfDate;
+    private Double term;
+    private Double value;
+    private Timestamp creationDate;
+}
---
+++ src/main/java/com/nnk/springboot/domain/Rating.java
@@ -0,0 +1,24 @@
+package com.nnk.springboot.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@Table(name = "rating")
+public class Rating {
+    // TODO: Map columns in data table RATING with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String moodysRating;
+    private String sandPRating;
+    private String fitchRating;
+    private Integer orderNumber;
+}
